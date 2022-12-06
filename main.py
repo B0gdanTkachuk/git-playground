a5067d01c79edae9eebc76a782ab7978cb0bea2b
B0gdanTkachuk <btkachuk@kse.org.ua>
2022-12-06 16:46:30 +0200

    added guesses list

--- main.py
+++ main.py
@@ -45,12 +45,15 @@ while not is_game_over():
         continue
 
     if guess in full_list:
-        guessed += 1
-        guesses.append(guess)
-        if guessed == WORDS_TO_WIN:
+        if guess in guesses:
+            print('You have already entered this word. Try again.')
+        else:
+         guessed += 1
+         guesses.append(guess)
+         if guessed == WORDS_TO_WIN:
             congratulate_user()
             exit()
-        print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
